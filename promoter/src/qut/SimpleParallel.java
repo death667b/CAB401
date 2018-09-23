657623b572a13d5b0e01c3362263023d1a479969
death667b <idaniel@spinnys.com>
2018-09-23 17:30:01 +1000

    Ran code cleanup on all files in src/qut

--- promoter/src/qut/SimpleParallel.java
+++ promoter/src/qut/SimpleParallel.java
@@ -1,23 +1,20 @@
 package qut;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 
-public class SimpleParallel extends Sequential
-{
+public class SimpleParallel extends Sequential {
     private static ReentrantLock lock = new ReentrantLock(true);
 
-    public static void run(String referenceFile, String dir) throws IOException
-    {
+    public static void run(String referenceFile, String dir) throws IOException {
         List<Gene> referenceGenes = ParseReferenceGenes(referenceFile);
-        for (String filename : ListGenbankFiles(dir))
-        {
+        for (String filename : ListGenbankFiles(dir)) {
             System.out.println(filename);
             GenbankRecord record = Parse(filename);
-            for (Gene referenceGene : referenceGenes)
-            {
+            for (Gene referenceGene : referenceGenes) {
                 System.out.println(referenceGene.name);
                 for (Gene gene : record.genes)
                     new Thread(new FindGeneRunnable(lock, gene, referenceGene, record, consensus)).start();
@@ -25,8 +22,7 @@ public class SimpleParallel extends Sequential
         }
     }
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         long startTime = System.nanoTime();
 
         run("referenceGenes.list", "Ecoli");
@@ -37,6 +33,6 @@ public class SimpleParallel extends Sequential
         for (Map.Entry<String, Sigma70Consensus> entry : consensus.entrySet())
             System.out.println(entry.getKey() + " " + entry.getValue());
 
-        System.out.println("Executing time in seconds: "+ timeElapsed / 1000000000);
+        System.out.println("Executing time in seconds: " + timeElapsed / 1000000000);
     }
 }
